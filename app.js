6d5bcbf4617ac40f9e9ca39fb9284fb6bb401430
Johanan Lai <lai.johanan@gmail.com>
2018-07-24 05:24:27 -0700

    Added delete, clear, and help commands

--- app.js
+++ app.js
@@ -14,82 +14,82 @@ client.on('ready', () => {
     // Event runs if bot is online and ready
     console.log(`Bot has started with ${client.users.size} users in ${client.channels.size} channels of ${client.guilds.size} guilds.`); 
 
-	// Job that recurrs every minute
+    // Job that recurrs every minute
     var rule = new schedule.RecurrenceRule();
     rule.minute = new schedule.Range(0, 59, 1);
 
     var job = schedule.scheduleJob(rule, function() {
-		// If timers channel has been set up
-		if (timersChannelId) {
-			timersChannel = client.channels.get(timersChannelId);
+        // If timers channel has been set up
+        if (timersChannelId) {
+            timersChannel = client.channels.get(timersChannelId);
 
             // Parse through scheduled events dict
-			var scheduledEventsEmbed;
+            var scheduledEventsEmbed;
             var scheduledEventsDescText = "```fix";
             var scheduledEventsChanText = "```css";
             var scheduledEventsTimeLeftText = "```yaml";
 
             for (var key in scheduledEvents) {
                 if (parseInt(scheduledEvents[key][3]) <= 0) {
-					// Remove any old scheduled events and add them to active
+                    // Remove any old scheduled events and add them to active
                     activeEvents[key] = [scheduledEvents[key][0], scheduledEvents[key][1], scheduledEvents[key][2]];
-					undoActiveEvent = true;
+                    undoActiveEvent = true;
                     delete scheduledEvents[key];
                 }
-				else {
-					// Format strings to display in scheduled events
+                else {
+                    // Format strings to display in scheduled events
                     scheduledEventsDescText += "\n" + String(scheduledEvents[key][0]) + "\n";
                     scheduledEventsChanText += "\n" + String(scheduledEvents[key][1]) + "\n";
                     scheduledEventsTimeLeftText += "\n" + String(scheduledEvents[key][3]) + "\n";
 
-					// Update time left
-                	scheduledEvents[key][3] = parseInt(scheduledEvents[key][3]) - 1;
-				}
+                    // Update time left
+                    scheduledEvents[key][3] = parseInt(scheduledEvents[key][3]) - 1;
+                }
             }
-			scheduledEventsDescText += "```";
+            scheduledEventsDescText += "```";
             scheduledEventsChanText += "```";
             scheduledEventsTimeLeftText += "```";
 
-		    // Parse through active events dict
-		    var activeEventsEmbed;
-		    var activeEventsDescText = "```fix";
-			var activeEventsChanText = "```css";
-		    var activeEventsTimeLeftText = "```yaml";
+            // Parse through active events dict
+            var activeEventsEmbed;
+            var activeEventsDescText = "```fix";
+            var activeEventsChanText = "```css";
+            var activeEventsTimeLeftText = "```yaml";
 
-		    for (var key in activeEvents) {
+            for (var key in activeEvents) {
                 if (parseInt(activeEvents[key][2]) <= 0) {
-					// Remove any old active events
+                    // Remove any old active events
                     delete activeEvents[key];
                 }
                 else {
                     // Format strings to display in active events
-		            activeEventsDescText += "\n" + String(activeEvents[key][0]) + "\n";
-				    activeEventsChanText += "\n" + String(activeEvents[key][1]) + "\n";
-		            activeEventsTimeLeftText += "\n" + String(activeEvents[key][2]) + "\n";
+                    activeEventsDescText += "\n" + String(activeEvents[key][0]) + "\n";
+                    activeEventsChanText += "\n" + String(activeEvents[key][1]) + "\n";
+                    activeEventsTimeLeftText += "\n" + String(activeEvents[key][2]) + "\n";
                     
                     // Update time left
                     activeEvents[key][2] = parseInt(activeEvents[key][2]) - 1;
                 }
-		    }
-		    activeEventsDescText += "```";
-			activeEventsChanText += "```";
-		    activeEventsTimeLeftText += "```";
-		
-			// Display different embeds if none in progress
-		    if (Object.keys(scheduledEvents).length > 0) {
-		        scheduledEventsEmbed = new Discord.MessageEmbed()
-		            .setTitle(":calendar: Scheduled events")
-		            .setColor(0xfbd204)
-		            .addField("Description", scheduledEventsDescText, true)
-					.addField("Channel", scheduledEventsChanText, true)
-		            .addField("Time until start", scheduledEventsTimeLeftText, true);
-		    }
-		    else {
-		        scheduledEventsEmbed = new Discord.MessageEmbed()
-		            .setTitle(":no_bell: No scheduled events")
-		            .setDescription("```Come back later, or ask for a GQ/buff to be popped!```")
-		            .setColor(0xffffff)
-		    }
+            }
+            activeEventsDescText += "```";
+            activeEventsChanText += "```";
+            activeEventsTimeLeftText += "```";
+        
+            // Display different embeds if none in progress
+            if (Object.keys(scheduledEvents).length > 0) {
+                scheduledEventsEmbed = new Discord.MessageEmbed()
+                    .setTitle(":calendar: Scheduled events")
+                    .setColor(0xfbd204)
+                    .addField("Description", scheduledEventsDescText, true)
+                    .addField("Channel", scheduledEventsChanText, true)
+                    .addField("Time until start", scheduledEventsTimeLeftText, true);
+            }
+            else {
+                scheduledEventsEmbed = new Discord.MessageEmbed()
+                    .setTitle(":no_bell: No scheduled events")
+                    .setDescription("```Come back later, or ask for a GQ/buff to be popped!```")
+                    .setColor(0xffffff)
+            }
 
             if (Object.keys(activeEvents).length > 0) {
                 activeEventsEmbed = new Discord.MessageEmbed()
@@ -106,12 +106,12 @@ client.on('ready', () => {
                     .setColor(0x990000)
             }
 
-			// Send embeds and delete after 1 min
-    	    timersChannel.send(activeEventsEmbed)
-		        .then(msg => msg.delete({timeout: 60000}))
-		        .catch(error => console.log(`Failed to delete message due to ${error}`));
+            // Send embeds and delete after 1 min
+            timersChannel.send(activeEventsEmbed)
+                .then(msg => msg.delete({timeout: 60000}))
+                .catch(error => console.log(`Failed to delete message due to ${error}`));
 
-			timersChannel.send(scheduledEventsEmbed)
+            timersChannel.send(scheduledEventsEmbed)
                 .then(msg => msg.delete({timeout: 60000}))
                 .catch(error => console.log(`Failed to delete message due to ${error}`));
         }
@@ -137,62 +137,91 @@ client.on('message', message => {
     const command = args.shift().toLowerCase();
     
     // Commands
-	if (command === "setup") {
-		if (!message.member.hasPermission("MANAGE_MESSAGES")) {
-			message.channel.send("You don't have permission to do this!");
-		}
-		else if (args.length != 1 || isNaN(parseInt(args[args.length-1]))) {
-			message.channel.send("Format: `!setup <discord channel id>`");
-		}
-		else {
-			timersChannelId = args[0];
-		}
-	}
+    if (command === "setup") {
+        if (!message.member.hasPermission("MANAGE_MESSAGES")) {
+            message.channel.send("You don't have permission to do this!");
+        }
+        else if (args.length != 1 || isNaN(parseInt(args[args.length-1]))) {
+            message.channel.send("Format: `!setup <discord channel id>`");
+        }
+        else {
+            timersChannelId = args[0];
+        }
+    }
 
-	if (command === "timer") {
-		if (args.length < 3 || isNaN(parseInt(args[args.length-1]))) {
-			message.channel.send("Format: `!timer <description> <channel> <event length in minutes>`");
-		}
-		else {
-			const desc = args.slice(0, args.length-2).join(" ");
-			const chan = args[args.length-2];
-			const timeLeft = parseInt(args[args.length-1]);
+    if (command === "timer") {
+        if (args.length < 3 || isNaN(parseInt(args[args.length-1]))) {
+            message.channel.send("Format: `!timer <description> <channel> <event length in minutes>`");
+        }
+        else {
+            const desc = args.slice(0, args.length-2).join(" ");
+            const chan = args[args.length-2];
+            const timeLeft = parseInt(args[args.length-1]);
 
-			activeEvents[desc + chan] = [desc, chan, timeLeft];
-			undoKey = desc + chan;
-			undoActiveEvent = true;
-		}
-	}
+            activeEvents[desc + chan] = [desc, chan, timeLeft];
+            undoKey = desc + chan;
+            undoActiveEvent = true;
+        }
+    }
 
     if (command === "schedule") {
-    	if (args.length < 4 || isNaN(parseInt(args[args.length-1])) || isNaN(parseInt(args[args.length-2]))) {
+        if (args.length < 4 || isNaN(parseInt(args[args.length-1])) || isNaN(parseInt(args[args.length-2]))) {
             message.channel.send("Format: `!schedule <description> <channel> <event length in minutes> <time until event starts in minutes>`");
         }
         else {
             const desc = args.slice(0, args.length-3).join(" ");
             const chan = args[args.length-3];
-			const eventLength = args[args.length-2];
+            const eventLength = args[args.length-2];
             const timeLeft = parseInt(args[args.length-1]);
 
             scheduledEvents[desc + chan] = [desc, chan, eventLength, timeLeft];
             undoKey = desc + chan;
-			undoActiveEvent = false;
+            undoActiveEvent = false;
         }
     }
 
-	if (command === "undo") {
-		if (undoActiveEvent) {
-			delete activeEvents[undoKey];
-		}
-		else {
-			delete scheduledEvents[undoKey];
-		}
-	}
+    if (command === "undo") {
+        if (undoActiveEvent) {
+            delete activeEvents[undoKey];
+        }
+        else {
+            delete scheduledEvents[undoKey];
+        }
+    }
+
+    if (command === "delete") {
+        if (args.length < 2) {
+            message.channel.send("Format: `!delete <original description> <original channel>`");
+        }
+        else {
+            const desc = args.slice(0, args.length-1).join(" ");
+            const chan = args[args.length-1];
+
+            delete activeEvents[desc + chan];
+        }
+    }
+  
+    if (command === "timershelp") {
+        if (args.length != 0) {
+            message.channel.send("Format: `!timershelp`");
+        }
+        else {
+            message.channel.send("**Commands**\n`!setup <discord channel id>`\n`!timer <description> <channel> <event length in minutes>`\n`!schedule <description> <channel> <event length in minutes> <time until event starts in minutes>`\n`!undo`\n`!delete <original description> <original channel>`\n`!timersclear`");
+        }
+    }
+  
+    if (command === "timersclear") {
+        if (message.member.hasPermission("MANAGE_MESSAGES") && timersChannel !== undefined) {
+            timersChannel.bulkDelete(100)
+                .then(messages => console.log(`Bulk deleted ${messages.size} messages`))
+                .catch(error => message.reply("Failed to bulk delete messages: ${error}"));
+        }
+    }
     
 //    if (command === "purge") {
-//		if (message.member.hasPermission("MANAGE_MESSAGES")) {
-//        	message.channel.bulkDelete(100)
-//        		.then(messages => console.log(`Bulk deleted ${messages.size} messages`))
+//      if (message.member.hasPermission("MANAGE_MESSAGES")) {
+//          message.channel.bulkDelete(100)
+//              .then(messages => console.log(`Bulk deleted ${messages.size} messages`))
 //                .catch(error => message.reply("Failed to bulk delete messages: ${error}"));
 //        }
 //    }
